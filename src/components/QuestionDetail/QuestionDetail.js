fa41c2fe4253cee339b123f8f5b208bfc94ae2c2
OzerOzturk <ozerozturk1636@gmail.com>
2022-08-25 20:54:54 +0300

    fix(QuestionDetail.js) fix fetching data url with useParams hook.

--- src/components/QuestionDetail/QuestionDetail.js
+++ src/components/QuestionDetail/QuestionDetail.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./QuestionDetail.css";
 
 const QuestionDetail = ({ match }) => {
   const [questionDetail, setQuestionDetail] = useState([]);
 
+  const { questionId } = useParams();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://polls.apiblueprint.org/?url=${match.params.url}`
+          `https://polls.apiblueprint.org/questions/${questionId}`
         );
         console.log(response);
         setQuestionDetail(response);
